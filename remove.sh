dfe2a5d792e16b7550845f29dd5c03a62b4664cd
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-03-04 10:49:29 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,4 +1,4 @@
-sudo apt update && sudo apt purge xfce4 xfce4-goodies && sudo apt purge tigervnc-standalone-server && vncserver -kill :1 && rm -rf ~/.vnc/xstartup;
+sudo apt update && sudo apt purge xfce4 xfce4-goodies && sudo apt purge tigervnc-standalone-server && vncserver -kill :* && rm -rf ~/.vnc/xstartup;
 rm -rf /etc/vnc;
 rm -rf $HOME/.Xresources;
 sudo ufw deny 5901 && sudo ufw enabled && sudo ufw reload;
