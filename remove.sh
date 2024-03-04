2a985a4dab0aa46a36b01756ad8b07c69e73be88
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-03-04 10:20:42 +0700

    Create remove.sh

---
+++ remove.sh
@@ -0,0 +1,6 @@
+sudo apt update && sudo apt purge xfce4 xfce4-goodies && sudo apt purge tigervnc-standalone-server && rm -rf ~/.vnc/xstartup;
+rm -rf /etc/vnc;
+rm -rf $HOME/.Xresources;
+sudo ufw deny 5901 && sudo ufw enabled && sudo ufw reload;
+echo "Vnc removed successfuly !"
+sleep 5;
