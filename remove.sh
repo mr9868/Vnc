4d9dd41a7c16988d86c6d1dda7156d993a0fa5ef
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-03-04 11:45:38 +0700

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -6,10 +6,45 @@ sudo ufw deny 5901 && sudo ufw enabled && sudo ufw reload;
 echo "Vnc removed successfuly !"
 sleep 10
 }
+
+#chrome remove function
+function remove_chrome() {
+  sudo apt-get purge google-chrome-stable && rm -rf ~/.config/google-chrome/ &&  sudo apt-get update;
+  echo "Chrome has successfuly removed !"
+  sleep 5
+}
+#end of chrome remove function
+
+#prompt for remove google chrome
+function chrome_prompt() {
+REQUIRED_PKG="some-package"
+PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+echo Checking for $REQUIRED_PKG: $PKG_OK
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  printf 'Do you want to remove chrome to your machine (y/n)? '
+    read answer
+  if [ "$answer" != "${answer#[Yy]}" ] ;then 
+      remove_chrome; 
+  else
+      clear;
+      echo "Chrome remove rejected !"
+      sleep 5
+  fi
+else
+      echo "Chrome doesn't installed on this machine !"
+      sleep 5
+fi
+  
+}
+#end of chrome remove prompt
+
+
 printf 'Do you want to remove vnc from your machine (y/n)? '
 read answer
 if [ "$answer" != "${answer#[Yy]}" ] ;then 
     remove;
+    chrome_prompt;
 else
     clear;
     echo "Nice !"
