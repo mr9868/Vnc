3665686117c58d08b798f67a8a4e32e91a0db5f5
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-03-04 13:02:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ function install(){
 #chrome install function
 function install_chrome(){
   apt install wget && wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb;
-  sudo dpkg -i google-chrome-stable_current_amd64.deb && sudo apt-get install -f 
+  sudo dpkg -i google-chrome-stable_current_amd64.deb && sudo apt-get install -f && sudo apt --fix-broken install
   echo "Chrome has successfuly installed !"
   sleep 5
 }
