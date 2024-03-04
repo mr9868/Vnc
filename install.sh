3bfc084f89917e2c29e56e2b1834b3d1de124004
Muhammad Ruly <116989757+mr9868@users.noreply.github.com>
2024-03-04 12:46:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,6 @@
 #install function
 function install(){
   sudo apt update && apt upgrade && sudo apt install xfce4 xfce4-goodies && sudo apt install tigervnc-standalone-server && vncserver && vncserver -kill :*;
-  cat << EOF > ~/.vnc/xstartup
-  #!/bin/sh
-  
-  # Start up the standard system desktop
-  unset SESSION_MANAGER
-  unset DBUS_SESSION_BUS_ADDRESS
-  
-  /usr/bin/startxfce4
-  
-  [ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
-  [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-  x-window-manager &
-  EOF
   
   chmod +x ~/.vnc/xstartup;
   sudo ufw allow 5901 && sudo ufw enabled && sudo ufw reload && vncserver -localhost no :1;
